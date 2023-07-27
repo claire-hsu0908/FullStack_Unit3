ba359bbc76696fc632c64aadad1f06e90671458c
Claire Hsu <claire.hsu837@duke.edu>
2023-07-27 12:56:14 -0400

    backend production

---
+++ Exercise 3.1 to 3.6.js
@@ -0,0 +1,104 @@
+const express = require('express')
+const app = express()
+//imports the http server from node.js
+
+app.use(express.json())
+
+let persons = [
+  { 
+    "id": 1,
+    "name": "Arto Hellas", 
+    "number": "040-123456"
+  },
+  { 
+    "id": 2,
+    "name": "Ada Lovelace", 
+    "number": "39-44-5323523"
+  },
+  { 
+    "id": 3,
+    "name": "Dan Abramov", 
+    "number": "12-43-234345"
+  },
+  { 
+    "id": 4,
+    "name": "Mary Poppendieck", 
+    "number": "39-23-6423122"
+  }
+]
+
+app.get('/',(request,response)=>{
+  response.send('<h1>Hello World!</h1>')
+})
+
+app.get('/api/persons',(request,response)=>{
+  response.json(persons)
+})
+
+app.get('/info',(request,response)=>{
+  response.timeStart = new Date()
+  response.send(`<p>Phonebook has info for ${persons.length} people
+  <br/>
+    ${response.timeStart}
+  <p> `)
+})
+
+app.get('/api/persons/:id',(request,response)=>{
+  const id = Number(request.params.id)
+  note = persons.find(person=>person.id===id)
+  if(note){
+    response.json(note)
+  }
+  else{
+    response.status(404).end()
+  }
+})
+
+app.delete('/api/persons/:id',(request,response)=>{
+  const id = Number(request.params.id)
+  persons = persons.filter(person=>person.id!==id)
+  response.send('Successfully deleted')
+  response.status(204).end()
+
+}
+)
+
+const newID = ()=>{
+  const min = Math.max(...persons.map(p=>p.id))
+  const ID = Math.floor(Math.random()*(100-min)+min)
+  console.log(Math.max(...persons.map(p=>p.id))+1)
+  console.log(ID)
+  return ID
+}
+
+app.post('/api/persons',(request,response)=>{
+  const body = request.body
+
+  if(!body.name||!body.number){
+    return response.status(400).json({
+      error:'name/number is missing'
+    })
+}
+
+ if(persons.find(person=>person.name===body.name)){
+  return response.status(400).json({
+    error:'name must be unique'
+  })
+ }
+
+  const personObject = {
+    id:newID(),
+    name:body.name,
+    number:body.number
+  }
+
+  persons = persons.concat(personObject)
+  response.json(personObject)
+})
+
+
+// const PORT = 3001
+// app.listen(PORT, ()=>{
+//     console.log(`Server running on port ${PORT}`)
+// }) //starts server and makes server receptible to incoming requests  
+
